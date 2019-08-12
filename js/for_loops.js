b20576ae21c206316a0af4e4abcf9a6e7b7b0319
Kyndall Sims <sims.kyndall22@gmail.com>
2019-08-12 14:34:38 -0500

    add external js

---
+++ js/for_loops.js
@@ -0,0 +1,6 @@
+function showMultiplicationTable(num) {
+    for (var i = 1; i <= 10; i++){
+        console.log(num*i);
+    }
+}
+showMultiplicationTable(7);
--- js/while.js
+++ js/while.js
@@ -1,4 +1,22 @@
-function timesTwo(num) {
-    while (num * 2) {
-    }
+//first exercise: done---------------------------------------
+function multiplyByTwo(num) {
+    return (num * 2);
 }
+console.log(multiplyByTwo(12));
+
+//second exercise: unfinished---------------------------------
+
+//icecream seller cant go home until everything is bought
+//use a do-while loop, log the amount of cones sold to each person
+//cannot sell more than she has, if cones are > what the person asked for say
+//"sorry! i only have x cones"
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+// function leftToSell(cones) {
+//     do {
+//
+//     }while
+// }
