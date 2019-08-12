2306f26966a801c8f2bab70374fc2ecfcfff8058
Kyndall Sims <sims.kyndall22@gmail.com>
2019-08-12 15:48:14 -0500

    kinda works?

--- js/for_loops.js
+++ js/for_loops.js
@@ -6,3 +6,40 @@ function showMultiplicationTable(num) {
 }
 showMultiplicationTable(7);
 
+//for loop problem 2 unfinished -----------------------------------------
+var i = Math.floor(Math.random() * 50) + 50;
+function isEvenOrOdd(num1) {
+    for(var i=0; i<=15; i++){
+        if (i === 0){
+            console.log(i + "is even");
+        }
+        else if (i % 2){
+            console.log(i + " is even");
+        } else{
+            console.log(i + " is odd");
+        }
+    }
+} isEvenOrOdd(7);
+//for loop problem 3 unfinished------------------------------------
+// function addOne(num) {
+//     for (var i = 0; i <=9; i++){
+//         console.log(num);
+//     }
+// }addOne(2);
+
+function generatePyramid(num) {
+    var number = '';
+
+    for (var i = 1; i <= num; i++) {
+        console.log(number += i);
+    }
+}
+generatePyramid(9);
+
+//for loops problem 4 unfinished--------------------------------
+function subtractFive(num) {
+    for (var i = 5; i <= 100; i--) {
+        console.log(num - i);
+        break;
+    }
+}subtractFive(100);
