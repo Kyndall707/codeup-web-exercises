0ad827b5782cbeeda644a942d179705cd9b01438
Kyndall Sims <sims.kyndall22@gmail.com>
2019-09-10 12:30:05 -0500

    working on second part of the exercise

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,36 @@
-function wait(num) {
-    return new Promise(function(){
-        setTimeout(function(){
-            resolve();
-        }, num)
-    });
-}
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+// function wait(num) {
+//     return new Promise(function(){
+//         setTimeout(function(){
+//             resolve();
+//         }, num)
+//     });
+// }
+//
+// // also works, just shorter syntax
+//
+// // function wait(num) {
+// //     return new Promise(function(){
+// //         setTimeout(resolve, num);
+// //     });
+// // }
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//
+//
+
+// practicing fetch
+//
+// function getGithubUsernames() {
+//     return fetch('https://api.github.com/users', {headers:{'Authorization': 'gitHubKey'}})
+//         .then(response => response.json())
+//         .then(users => users.map(user => user.login));
+// }
+//
+// // later on...
+//
+// getGithubUsernames().then((usernames) => {
+//     usernames.forEach((username) => {
+//         // do something with each username
+//     });
+// }).catch(error => console.error(error));
